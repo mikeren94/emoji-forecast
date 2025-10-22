4a7d03d90639c7d63261cf37b8fa005b09ff871e
Michael Rennison <mike.j.renn@gmail.com>
2025-10-22 13:58:40 +0100

    Added handle city selection

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { useState } from 'react';
 import CitySearchBar from './components/CitySearchBar';
 import logo from './logo.svg';
 import './scss/App.scss';
 import '@progress/kendo-theme-default/dist/all.css';
 function App() {
+  const [city, setCity] = useState('');
+
   return (
     <div className="App">
       <header className="App-header">
-        <CitySearchBar />
+        <CitySearchBar 
+          setSelectedCity={setCity}
+        />
+        <p>Getting data for {city}</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/components/CitySearchBar.jsx
+++ src/components/CitySearchBar.jsx
@@ -2,14 +2,19 @@ import { getAutoCompleteCities } from "../services/google-service";
 import { useState } from "react";
 import { AutoComplete } from "@progress/kendo-react-dropdowns";
 
-function CitySearchBar() {
+function CitySearchBar({setSelectedCity}) {
     const [cities, setCities] = useState([])
     const [inputValue, setInputValue] = useState("");
 
     const onCityChange = (e) => {
         setInputValue(e.value)
         fetchCities();
+
+        if (cities.includes(e.value)) {
+            handleCitySelect(e.value);
+        }
     }
+
     const fetchCities = async () => {
         if (!inputValue) return;
 
@@ -20,6 +25,10 @@ function CitySearchBar() {
             return `${main}, ${secondary}`;
         });
         setCities(cityCountryList);
+    }
+
+    const handleCitySelect = (city) => {
+        setSelectedCity(city)
     }
 
     return (
