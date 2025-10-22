ff54dc90d0c552782eb7151b764b6ff9629ac8a2
Michael Rennison <mike.j.renn@gmail.com>
2025-10-22 12:19:57 +0100

    Added .env file

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import CitySearchBar from './components/CitySearchBar';
 import logo from './logo.svg';
 import './scss/App.scss';
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <CitySearchBar />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/CitySearchBar.jsx
@@ -0,0 +1,7 @@
+function CitySearchBar({}) {
+    return (
+        <div>City Search Bar</div>
+    )
+}
+
+export default CitySearchBar;
